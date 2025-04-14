e5e10fef671c690908079840ad3fd3a50b3e94d0
Mike Hardy <hardym30@gmail.com>
2025-04-14 18:32:47 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,34 +2,41 @@ const apiKey = '4c143b18';
 const apiURL = 'https://www.omdbapi.com/';
 
 const searchMovies = async () => {
-  const searchInput = document.querySelector('#search-input');
-  const listMovie = document.querySelector('#list-movie');
+    const searchInput = document.querySelector('#search-input');
+    const listMovie = document.querySelector('#list-movie');
+    listMovie.innerHTML = 'The Dude Abides...';
+  
+    const query = searchInput.value.trim();
+    if (!query) return;
+  
+    setTimeout(async () => {
+      try {
+        const data = await fetch(`${apiURL}?apiKey=${apiKey}&s=${encodeURIComponent(query)}`)
+          .then(res => res.json());
+  
+        if (data.Response === 'True') {
+          listMovie.innerHTML = data.Search.map(movie => `
+            <div class="container">
+              <div class="row" id="${movie.imdbID}">
+                <img src="${movie.Poster}" class="movie__img">
+                <div class="card-body">
+                  <h5 class="movie__title">${movie.Title}</h5>
+                  <h6 class="movie__year">${movie.Year}</h6>
+                </div>
+              </div>
+            </div>
+          `).join('');
+        } else {
+          listMovie.innerHTML = `<h1 class="text-center">${data.Error}</h1>`;
+        }
+      } 
+      catch (error) {
+        listMovie.innerHTML = `<h1 class="text-center">Something is seriously wrong.</h1>`;
+        console.error(error);
+      }
+    }, 1000); 
+  };
 
-  const query = searchInput.value.trim();
-  if (!query) return;
-
-  listMovie.innerHTML = 'Loading...';
-
-  const res = await fetch(`${apiURL}?apiKey=${apiKey}&s=${query}`);
-  const data = await res.json();
-
-  if (data.Response === 'True') {
-    listMovie.innerHTML = data.Search.map(movie => `
-      <div class="col-md-4">
-        <div class="card my-2" data-id="${movie.imdbID}">
-          <img src="${movie.Poster}" class="card-img-top">
-          <div class="card-body">
-            <h5 class="card-title">${movie.Title}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${movie.Year}</h6>
-          </div>
-        </div>
-      </div>
-    `).join('');
-  } else {
-    listMovie.innerHTML = `<h1 class="text-center">${data.Error}</h1>`;
-  }
-
-};
 
 
 document.querySelector('#search-button').addEventListener('click', searchMovies);
@@ -42,3 +49,6 @@ document.querySelector('#list-movie').addEventListener('click', e => {
   const card = e.target.closest('.card');
   if (card?.dataset.id) showMovieDetails(card.dataset.id);
 });
+
+
+
